f6fed0adfde216085e325971919dcdc65f479873
baxlash <yaroslav.dimitrov@outlook.com>
2016-01-26 10:28:36 +0200

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.  On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	new file:   .vs/150117_Exam/v14/.suo
    	modified:   01._Cake_Tycoon/bin/Debug/01._Cake_Tycoon.exe
    	new file:   01._Cake_Tycoon/bin/Debug/01._Cake_Tycoon.pdb
    	new file:   01._Cake_Tycoon/bin/Debug/01._Cake_Tycoon.vshost.exe
    	new file:   01._Cake_Tycoon/bin/Debug/01._Cake_Tycoon.vshost.exe.manifest
    	new file:   01._Cake_Tycoon/obj/x86/Debug/01._Cake_Tycoon.csproj.FileListAbsolute.txt
    	new file:   01._Cake_Tycoon/obj/x86/Debug/01._Cake_Tycoon.csprojResolveAssemblyReference.cache
    	new file:   01._Cake_Tycoon/obj/x86/Debug/01._Cake_Tycoon.exe
    	new file:   01._Cake_Tycoon/obj/x86/Debug/01._Cake_Tycoon.pdb
    	new file:   01._Cake_Tycoon/obj/x86/Debug/DesignTimeResolveAssemblyReferencesInput.cache
    	new file:   02. Beer Stock/02. Beer Stock.csproj
    	new file:   02. Beer Stock/App.config
    	new file:   02. Beer Stock/BeerStock.cs
    	new file:   02. Beer Stock/Properties/AssemblyInfo.cs
    	new file:   02. Beer Stock/bin/Debug/_02.Beer_Stock.exe
    	new file:   02. Beer Stock/bin/Debug/_02.Beer_Stock.exe.config
    	new file:   02. Beer Stock/bin/Debug/_02.Beer_Stock.pdb
    	new file:   02. Beer Stock/bin/Debug/_02.Beer_Stock.vshost.exe
    	new file:   02. Beer Stock/bin/Debug/_02.Beer_Stock.vshost.exe.config
    	new file:   02. Beer Stock/bin/Debug/_02.Beer_Stock.vshost.exe.manifest
    	new file:   02. Beer Stock/obj/Debug/02. Beer Stock.csproj.FileListAbsolute.txt
    	new file:   02. Beer Stock/obj/Debug/02. Beer Stock.csprojResolveAssemblyReference.cache
    	new file:   02. Beer Stock/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache
    	new file:   02. Beer Stock/obj/Debug/TemporaryGeneratedFile_036C0B5B-1481-4323-8D20-8F5ADCB23D92.cs
    	new file:   02. Beer Stock/obj/Debug/TemporaryGeneratedFile_5937a670-0e60-4077-877b-f7221da3dda1.cs
    	new file:   02. Beer Stock/obj/Debug/TemporaryGeneratedFile_E7A71F73-0F8D-4B9B-B56E-8E70B10BC5D3.cs
    	new file:   02. Beer Stock/obj/Debug/_02.Beer_Stock.exe
    	new file:   02. Beer Stock/obj/Debug/_02.Beer_Stock.pdb
    	modified:   150117_Exam.sln
    
     Changes not staged for commit:
    	modified:   .vs/150117_Exam/v14/.suo

---
+++ Programming Basics/00.Test Exams/01. Exam 17-01-2015/150117_Exam/02. Beer Stock/BeerStock.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+class BeerStock
+{
+    static void Main()
+    {
+        string currentInput = string.Empty;
+        int reservedBeers = int.Parse(Console.ReadLine());
+        long beers, sixPacks, cases;
+        beers = 0;
+        sixPacks = 0;
+        cases = 0;
+
+        do
+        {
+            currentInput = Console.ReadLine();
+            string[] splited = currentInput.Split(' ');
+            switch (splited[1])
+            {
+                case "Beers":
+                    beers += long.Parse(splited[0]);
+                    break;
+                case "SixPacks":
+                    sixPacks += long.Parse(splited[0]);
+                    break;
+                case "Cases":
+                    cases += long.Parse(splited[0]);
+                    break;
+                default:
+                    break;
+            }
+            //Console.WriteLine(beers);
+            //Console.WriteLine(sixPacks);
+            //Console.WriteLine(cases);
+        } while (currentInput != "Exam Over");
+
+        beers += sixPacks * 6 + cases * 24 - beers / 100;
+
+        if (beers<reservedBeers)
+        {
+            long difference = reservedBeers - beers;
+            long insufficientCases = difference / 24;
+            long insufficientSixPacks = (difference % 24) / 6;
+            long insufficientBeers = (difference % 24) % 6;
+
+            Console.WriteLine(
+                "Not enough beer. Beer needed: {0} cases, {1} sixpacks and {2} beers.", 
+                insufficientCases,
+                insufficientSixPacks,
+                insufficientBeers
+                );
+        }
+        else
+        {
+            long difference = beers - reservedBeers;
+            long casesLeft = difference / 24;
+            long sixPacksLeft = (difference % 24) / 6;
+            long beersLeft = (difference % 24) % 6;
+
+            Console.WriteLine(
+                "Cheers! Beer left: {0} cases, {1} sixpacks and {2} beers.",
+                casesLeft,
+                sixPacksLeft,
+                beersLeft);
+
+        }
+    }
+}
