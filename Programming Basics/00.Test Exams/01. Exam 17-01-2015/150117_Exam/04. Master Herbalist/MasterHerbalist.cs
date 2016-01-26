774332d07adeadc9a8e936ece2e600117b494e4c
baxlash <yaroslav.dimitrov@outlook.com>
2016-01-26 13:42:11 +0200

    Commit

---
+++ Programming Basics/00.Test Exams/01. Exam 17-01-2015/150117_Exam/04. Master Herbalist/MasterHerbalist.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class MasterHerbalist
+{
+    static void Main(string[] args)
+    {
+        string currentInput = string.Empty;
+        int dailyExpenses = int.Parse(Console.ReadLine());
+        //int dailyBalance = 0;
+
+        while (currentInput != "Season Over")
+        {
+            int hours = 0;
+            //int herbs = 0;
+            int price = 0;
+            currentInput = Console.ReadLine();
+            string[] splited = currentInput.Split(' ');
+            hours = int.Parse(splited[0]);
+            price = int.Parse(splited[2]);
+            string path = splited[1];
+
+            for (int i = 0; i < hours; i++)
+            {
+                int index = i;
+                if (index>path.Length-1)
+                {
+                    index -= path.Length;
+                }
+
+                Console.WriteLine(path[index]);
+               /* if (path[index] == 'H')
+                {
+                    herbs++;
+                }*/
+            }
+
+        }
+    }
+}
