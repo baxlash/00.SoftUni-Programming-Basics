249b640b60c4a6d6329c4173cae7390364828f78
baxlash <yaroslav.dimitrov@outlook.com>
2016-01-26 15:13:02 +0200

    Final Herb

--- Programming Basics/00.Test Exams/01. Exam 17-01-2015/150117_Exam/04. Master Herbalist/MasterHerbalist.cs
+++ Programming Basics/00.Test Exams/01. Exam 17-01-2015/150117_Exam/04. Master Herbalist/MasterHerbalist.cs
@@ -6,34 +6,51 @@ class MasterHerbalist
     {
         string currentInput = string.Empty;
         int dailyExpenses = int.Parse(Console.ReadLine());
-        //int dailyBalance = 0;
+        double balance = 0;
+        int days = 0;
 
-        while (currentInput != "Season Over")
+        while (true)
         {
             int hours = 0;
-            int herbs = 0;
-            int price = 0;
+            int dailyHerbs = 0;
+            int dailyPrice = 0;
             currentInput = Console.ReadLine();
+            if (currentInput == "Season Over")
+            {
+                break;
+            }
+
             string[] splited = currentInput.Split(' ');
             hours = int.Parse(splited[0]);
-            price = int.Parse(splited[2]);
+            dailyPrice = int.Parse(splited[2]);
             string path = splited[1];
 
             for (int i = 0; i < hours; i++)
             {
                 int index = i;
-                if (index>path.Length-1)
+                if (index > path.Length - 1)
                 {
-                    index -= path.Length;
+                    index %= path.Length;
                 }
 
-                Console.WriteLine(path[index]);
                 if (path[index] == 'H')
                 {
-                    herbs++;
+                    dailyHerbs++;
                 }
             }
-
+            int dailyIncome = dailyPrice * dailyHerbs;
+            int dailyBalance = dailyIncome - dailyExpenses;
+            balance += dailyBalance;
+            days++;
+        }
+        if (balance > 0)
+        {
+            double avarageBalance = balance / days;
+            Console.WriteLine("Times are good. Extra money per day: {0:F2}.", avarageBalance);
+        }
+        else
+        {
+            Console.WriteLine("We are in the red. Money needed: {0}.", balance * (-1));
         }
     }
 }
