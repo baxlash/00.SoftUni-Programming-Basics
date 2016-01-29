539cbbb0173df7cfa8d3a5989d9f3d1d1700c7f8
baxlash <yaroslav.dimitrov@outlook.com>
2016-01-29 08:25:05 +0200

    edit

--- Programming Basics/05. Operators and Expressions/03. Homework/HW5_Operators_And_Expressions/03._Divided_By_7_And_5/DividedBy7And5.cs
+++ Programming Basics/05. Operators and Expressions/03. Homework/HW5_Operators_And_Expressions/03._Divided_By_7_And_5/DividedBy7And5.cs
@@ -6,7 +6,7 @@ class DividedBy7And5
 	{
 		Console.Write ("Enter a number: ");
 		int number = int.Parse (Console.ReadLine ());
-		bool result = number % 35 != 0;
+		bool result = number % 35 == 0;
 
 		Console.WriteLine ("Could the number {0} be divided by 7 and 5? - {1}", number, result);
 
