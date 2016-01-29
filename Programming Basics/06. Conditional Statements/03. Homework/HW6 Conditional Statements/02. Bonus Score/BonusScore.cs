6fd5f12e9ec641b43b040e9e897f2f0d067acbb5
baxlash <yaroslav.dimitrov@outlook.com>
2016-01-29 23:09:09 +0200

    HW 6

---
+++ Programming Basics/06. Conditional Statements/03. Homework/HW6 Conditional Statements/02. Bonus Score/BonusScore.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class BonusScore
+{
+    static void Main()
+    {
+        Console.Write("Enter a number between 1 and 9: ");
+        int number = int.Parse(Console.ReadLine());
+
+        switch (number)
+        {
+            case 1:
+                Console.WriteLine(number*10);
+                break;
+            case 2:
+                Console.WriteLine(number*10);
+                break;
+            case 3:
+                Console.WriteLine(number * 10);
+                break;
+            case 4:
+                Console.WriteLine(number * 100);
+                break;
+            case 5:
+                Console.WriteLine(number * 100);
+                break;
+            case 6:
+                Console.WriteLine(number * 100);
+                break;
+            case 7:
+                Console.WriteLine(number * 1000);
+                break;
+            case 8:
+                Console.WriteLine(number * 1000);
+                break;
+            case 9:
+                Console.WriteLine(number * 1000);
+                break;
+            default:
+                Console.WriteLine("invalid score");
+                break;
+        }
+    }
+}
---
+++ Programming Basics/06. Conditional Statements/03. Homework/HW6 Conditional Statements/04. Multiplication SIgn/MultiplicationSIgn.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class MultiplicationSIgn
+{
+    static void Main()
+    {
+        Console.Write("Enter the first number: ");
+        double firstNumber = double.Parse(Console.ReadLine());
+        Console.Write("Enter the second number: ");
+        double secondNumber = double.Parse(Console.ReadLine());
+        Console.Write("Enter the third number: ");
+        double thirdNumber = double.Parse(Console.ReadLine());
+
+        double[] numbers = { firstNumber, secondNumber, thirdNumber };
+        bool isNegative = false;
+        bool isZero = false;
+
+        foreach (double number in numbers)
+        {
+            if (number == 0)
+            {
+                isZero = true;
+                break;
+            }
+            else if (number < 0)
+            {
+                isNegative = !isNegative;
+            }
+        }
+
+        if (isZero)
+        {
+
+            Console.WriteLine("0");
+        }
+        else if (isNegative)
+        {
+            Console.WriteLine("-");
+        }
+        else
+        {
+            Console.WriteLine("+");
+        }
+    }
+}
