10a2b4c00a32d0d6005d10935aac8893237835bf
baxlash <yaroslav.dimitrov@outlook.com>
2016-01-31 23:26:53 +0100

    HW7 (1st commit)

---
+++ Programming Basics/07. Loops/HW7 - Loops/05. Calculation/Calculation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Numerics;
+
+class Calculation
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter n: ");
+        int n = int.Parse(Console.ReadLine());
+        Console.Write("Enter x: ");
+        int x = int.Parse(Console.ReadLine());
+
+        double result = 1;
+
+        for (int i = 1; i <= n; i++)
+        {
+            result += fact(i) / Math.Pow(x, i);
+        }
+
+        Console.WriteLine("{0:F5}",result);
+    }
+
+    static int fact(int n)
+    {
+        if (n==1)
+        {
+            return 1;
+        }
+        else
+        {
+            return n * fact(n - 1);
+        }
+    }
+
+    //static int pow (int b, int p)
+    //{
+    //    int res = 1;
+    //    for (int i = 1; i <= p; i++)
+    //    {
+    //        res *= b;
+    //    }
+    //    return res;
+    //}
+}
